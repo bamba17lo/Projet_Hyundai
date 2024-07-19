1859a3cc928d35b22b439bd123f39938189f1dc1
Bamba Lo <khadim221lo@gmail.com>
2024-07-19 12:08:32 +0000

    creation des diffrent pages

---
+++ Controllers/OffreController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Projet_Exam_hyundai.Controllers
+{
+    public class OffreController : Controller
+    {
+            public IActionResult Offre()
+            {
+                return View();
+            }
+        
+    }
+}
